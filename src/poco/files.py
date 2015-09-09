691d128056937a720c90e5cfd246947bff5934ca
brokkr <mail@madskas.dk>
2015-09-09 10:21:55 +0200

    removing all id3 tagging (for now) due to eyed3 changes

--- src/poco/files.py
+++ src/poco/files.py
@@ -2,19 +2,10 @@ import os
 import shutil
 import logging
 
-import eyeD3
+import eyed3
 from urlgrabber import urlgrab, progress
 
 from poco import errors
-
-_settings_synonyms = { \
-    '2.3': eyeD3.ID3_V2_3, \
-    '2.4': eyeD3.ID3_V2_4, \
-    'latin-1': eyeD3.LATIN1_ENCODING, \
-    'utf-16': eyeD3.UTF_16_ENCODING, \
-    'utf-16be': eyeD3.UTF_16BE_ENCODING, \
-    'utf-8': eyeD3.UTF_8_ENCODING \
-    }
 
 def delete_audio_file(entry_dic, sub_dic):
     '''Deletes one file'''
@@ -38,38 +29,6 @@ def download_audio_file(entry_dic, sub_dic, args_ns):
     # how to test if the right file was downloaded?
     # check file length? file name? 
     # not fool-proof as these could have been provided by the program itself...
-
-def tag_audio_file(sets_dic, entry_dic, sub_dic):
-    '''Tags one audio file with supplied metadata'''
-    localfile = _get_path(entry_dic, sub_dic)
-    container = eyeD3.Tag()
-
-    if not eyeD3.isMp3File(localfile) or not container.link(localfile):
-        error = 'The file ' + localfile + ' could not be tagged. '
-        suggest = ['Please check to see if the feed delivers valid mp3 files.']
-        errors.errors(error, suggest, fatal=False, title=sub_dic['title'].upper())
-        return
-
-    tags = {}
-    tags['artist'] = container.getArtist()
-    tags['album'] = container.getAlbum()
-    tags['genre'] = container.getGenre()
-    tags.update(sub_dic['metadata'])
-
-    if sets_dic['metadata']['removeid3v1'] == 'yes':
-        container.remove(eyeD3.ID3_V1)
-    id3version = _settings_synonyms[sets_dic['metadata']['id3version']]
-    id3encoding = _settings_synonyms[sets_dic['metadata']['id3encoding']]
-    container.header.setVersion(id3version)
-    container.setTextEncoding(id3encoding)
-
-    container.setArtist(tags['artist'])
-    container.setAlbum(tags['album'])
-    container.setGenre(tags['genre'])
-    try:
-        container.update(id3version)
-    except UnicodeEncodeError:
-        pass
 
 def check_path(sub_dic):
     '''Creates one directory'''
