cb397aab67daaf936ef55d86179c61229759a4a0
brokkr <mail@madskas.dk>
2015-09-10 09:54:57 +0200

    updating authorship

--- src/poco/__init__.py
+++ src/poco/__init__.py
@@ -5,11 +5,11 @@
 # of the GNU General Public License as published by the Free Software Foundation, \
 # either version 3 of the License, or (at your option) any later version.
 
-VERSION = "0.1.1"
-MAINTAINER = "Mads Michelsen <madchine@gmail.com>"
+VERSION = "0.2"
+MAINTAINER = "Mads Michelsen <reannual@gmail.com>"
 DESCRIPTION = "A cron-friendly, disk-space-conscious, command line podcast \
 aggregator, written in Python"
-URL = "http://code.google.com/p/poca"
+URL = "https://github.com/brokkr/poca"
 
 import pathsconfig
 import argsconfig
