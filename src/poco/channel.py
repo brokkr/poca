38abb14df938573718e9a6e723e62ee292804917
Mads Michelsen <mail@brokkr.net>
2016-02-29 07:44:00 +0100

    Added comments and function defs

--- src/poco/channel.py
+++ src/poco/channel.py
@@ -22,19 +22,21 @@ from poco import files
 
 class Channel:
     def __init__(self, config, logger, sub):
-        '''A class for a single subscription/channel'''
+        '''A class for a single subscription/channel. Creates the containers
+        first, then acts on them and updates the db as it goes.'''
         self.sub, self.logger = sub, logger
         self.logger.info(sub.title.upper())
-        # Find all available entries
+
         self.feed = Feed(self.sub)
         self.jar = history.get_jar(config.paths, self.sub)
         self.combo = Combo(self.feed, self.jar)
-        # Find all the ones we want (and don't want)
         self.wanted = Wanted(self.sub, self.combo, logger)
-        self.unwanted = Unwanted(self.jar, self.wanted)
-        # Act on the ones we have and those we don't for each category
-        for uid in self.unwanted.lst:
-            self.remove(uid, self.unwanted.dic[uid])
+        #self.unwanted = Unwanted(self.jar, self.wanted)
+
+        for uid in [ uid for uid in jar.lst if uid not in wanted.lst ]:
+            self.remove(uid, self.jar.dic[uid])
+        #for uid in self.unwanted.lst:
+        #    self.remove(uid, self.unwanted.dic[uid])
         self.new_jar = history.Jar(config.paths, self.sub)
         for uid in self.wanted.lst:
             entry = self.wanted.dic[uid]
@@ -45,25 +47,28 @@ class Channel:
             self.new_jar.lst.append(uid)
             self.new_jar.dic[uid] = entry
             self.new_jar.save()
-        # should any by mistake remain in the old jar (i.e. not have been 
+        # should any - by mistake - remain in the old jar (i.e. not have been 
         # either removed or renewed) we add them to the new jar. 
         for uid in self.jar.lst:
             self.check(uid, self.jar.dic[uid], config.args)
         logger.info('')
 
     def remove(self, uid, entry):
+        '''Deletes the file and removes the entry from the old jar'''
         self.logger.info(' Removing existing file:  ' + entry['poca_filename'])
-        # first we delete the actual file
         files.delete_file(entry['poca_abspath'])
-        # then remove the entry from the jar list and dictionary
         self.jar.lst.remove(uid)
         dummy = self.jar.dic.pop(uid)
         self.jar.save()
 
     def get(self, uid, entry, args):
+        '''Calls the download function with args so it'll know whether to 
+        display a progress meter or do a silent download'''
         files.download_audio_file(args, entry)
 
     def check(self, uid, entry, args):
+        '''Performs a quick check-up on existing keeper-files and removes 
+        the entry from the old jar'''
         self.logger.info(' Checking existing file:  ' + entry['poca_filename'])
         if not path.isfile(entry['poca_abspath']):
             self.get(uid, args)
@@ -72,13 +77,17 @@ class Channel:
 
 class Feed:
     def __init__(self, sub):
+        '''Constructs a container for feed entries'''
         doc = feedparser.parse(sub.url)
+        # note: some feeds do not have entry id's (?) The times bugle (old)
+        # for one failed. Check feedparser documentation.
         self.lst = [ entry.id for entry in doc.entries ]
         self.dic = { entry.id : entry for entry in doc.entries }
 
 class Combo:
     def __init__(self, feed, jar):
-        '''Copies feed then adds non-duplicates from jar'''
+        '''Constructs a container holding all combined feed and jar 
+        entries. Copies feed then adds non-duplicates from jar'''
         self.lst = list(feed.lst)
         self.lst.extend(x for x in jar.lst if x not in feed.lst)
         self.dic = feed.dic.copy()
@@ -86,6 +95,8 @@ class Combo:
 
 class Wanted():
     def __init__(self, sub, combo, logger):
+        '''Constructs a container for all the entries we have room for, 
+        regardless of where they are, internet or local folder.'''
         self.lst, self.dic = ( [], {} )
         mega = float(1024 * 1024)
         self.max_bytes = int(sub.max_mb) * mega
@@ -100,7 +111,8 @@ class Wanted():
                 entry['poca_url'] = entry.enclosures[0]['href']
                 entry['poca_size'] = uid_bytes
                 entry['poca_filename'] = self.get_filename(entry)
-                entry['poca_abspath'] = path.join(sub.sub_dir, entry['poca_filename'])
+                entry['poca_abspath'] = path.join(sub.sub_dir, 
+                    entry['poca_filename'])
                 self.dic[uid] = entry
                 logger.info(' Adding to wanted list:   ' + entry.title + 
                     ' @ ' + str(round(uid_bytes / mega, 2)) + ' Mb')
@@ -111,31 +123,39 @@ class Wanted():
             str(round(self.max_bytes / mega, 2)) + ' Mb')
 
     def get_size(self, entry):
+        '''Returns the entrys size in bytes. Tries to get if off of the
+        enclosure, otherwise pokes url for info.'''
         try:
             size = int(entry.enclosures[0]['length'])
         except KeyError:
             if entry.has_key('poca_size'):
                 size = entry['poca_size']
             else:
+                # why are we encoding the url as ascii? a requirement 
+                # of urllib? Also we already have extracted the url to
+                # 'poca_url'?
                 url = entry.enclosures[0]['href'].encode('ascii')
                 size = int(self.get_file_info(url, 'Content-Length'))
         return size
 
     def get_file_info(self, url, info_key):
         '''Gets stats about the file to be downloaded, such as file size'''
+        # could we use a 'with' statement here instead?
         f = urllib2.urlopen(url)
         value = f.info()[info_key]
         f.close()
         return value
 
     def get_filename(self, entry):
-        '''Parses URL to find base filename'''
+        '''Parses URL to find base filename. To be expanded with naming
+        options'''
         parsed_url = urlparse(entry['poca_url'])
         filename = path.basename(parsed_url.path)
         return filename
 
 class Unwanted:
     def __init__(self, jar, wanted):
+        '''Constructs a container for entries to be purged'''
         self.lst = [ uid for uid in jar.lst if uid not in wanted.lst ]
         self.dic = { uid : jar.dic[uid] for uid in self.lst }
 
