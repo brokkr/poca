0536457f7bd14f0e46ce6c559d8c0dbac2acf56d
Mads Michelsen <mail@brokkr.net>
2016-03-01 22:25:57 +0100

    setup gets new copyright notice and unicode

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-# Copyright 2010, 2011, 2016 Mads Michelsen (mail@brokkr.net)
+# -*- coding: utf-8 -*-
+# Copyright 2010-2016 Mads Michelsen (mail@brokkr.net)
 # 
 # This file is part of Poca.
-# Poca is free software: you can redistribute it and/or modify it under the terms \
-# of the GNU General Public License as published by the Free Software Foundation, \
-# either version 3 of the License, or (at your option) any later version.
+# Poca is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published by 
+# the Free Software Foundation, either version 3 of the License, 
+# or (at your option) any later version.
+
 
 from distutils.core import setup
 
