e5c288994b1b127129a1902b5a1b650a942a7f5f
Mads Michelsen <mail@brokkr.net>
2016-08-18 11:14:07 +0200

    starting up new branch due to weirdass github error

--- src/poco/args.py
+++ src/poco/args.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2010-2016 Mads Michelsen (mail@brokkr.net)
-# 
+#  
 # This file is part of Poca.
 # Poca is free software: you can redistribute it and/or modify it 
 # under the terms of the GNU General Public License as published by 
