4e9570c1dfb0e668ddd41d814011a8337885ec65
Mads Michelsen <mail@brokkr.net>
2016-08-30 10:25:45 +0200

    revised version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from distutils.core import setup
 
 setup(
 name = 'poca', 
-version = 'rev153',
+version = 'rev157',
 license = 'GPL3', 
 description = 'a command line podcast client', 
 long_description = 'A cron-friendly, disk-space-conscious, command line'
--- src/poco/version.py
+++ src/poco/version.py
@@ -1,1 +1,1 @@
-__version__ = 'rev153'
+__version__ = 'rev157'
