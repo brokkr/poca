35efe9f497b0a52ae7dce940b7afcd9135673510
Mads Michelsen <mail@brokkr.net>
2017-05-23 20:11:09 +0200

    instance is conf, not config

--- src/poco/about.py
+++ src/poco/about.py
@@ -9,7 +9,7 @@
 
 """Basic info"""
 
-__version__ = 'rev301'
+__version__ = 'rev302'
 VERSION = __version__
 MAINTAINER = "Mads Michelsen <mail@brokkr.net>"
 DESCRIPTION = "A cron-friendly command line podcast aggregator"
