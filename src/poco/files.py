b1417b16e4cea294482291be147abcf1b4ab0a37
Mads Michelsen <mail@brokkr.net>
2017-06-22 10:36:53 +0200

    cleaning out old junk from files

--- src/poco/files.py
+++ src/poco/files.py
@@ -10,76 +10,12 @@
 """File operations"""
 
 import os
-import sys
 import shutil
-import socket
-import urllib.request
-import urllib.error
 import requests
 
 from poco.outcome import Outcome
 
-
-
-def old_download_file(url, file_path, settings):
-    '''Download function with block time outs'''
-    try:
-        request = urllib.request.Request(url)
-        u = urllib.request.urlopen(request)
-    except urllib.error.HTTPError as e:
-        if not settings.useragent:
-            return Outcome(False, str(e))
-        try:
-            fakeheaders = {"User-Agent" : settings.useragent.text}
-            request = urllib.request.Request(url, headers=fakeheaders)
-            u = urllib.request.urlopen(request)
-        except urllib.error.HTTPError as e:
-            return Outcome(False, str(e))
-    except socket.error as e:
-        return Outcome(False, str(e))
-
-    f = open(file_path, "wb")
-
-    block_size = 8192
-    while True:
-        try:
-            download_block(u, f, block_size)
-        except NoMoreBufferException as e:
-            outcome = Outcome(True, str(e))
-            f.close()
-            break
-        except TimesUpException as e:
-            outcome = Outcome(False, str(e))
-            f.close()
-            del_outcome = delete_file(file_path)
-            break
-        except KeyboardInterrupt:
-            f.close()
-            del_outcome = delete_file(file_path)
-            sys.exit()
-
-    return outcome
-
-def old_download_img_file(url, sub_dir, settings):
-    '''Download an image file'''
-    try:
-        u = urllib.request.urlopen(url)
-    except urllib.error.URLError:
-        return Outcome(False, 'Couldnt get image')
-    subtype = u.headers.get_content_subtype()
-    u.close()
-    mime_dic = {'bmp': '.bmp', 'gif': '.gif', 'png': '.png',
-                'jpeg': '.jpg', 'jpg': '.jpg', 'webp': '.webp'}
-    extension = mime_dic[subtype] if subtype in mime_dic else None
-    if extension is None:
-        return Outcome(False, 'Bad image MIME type')
-    else:
-        file_path = os.path.join(sub_dir, 'cover' + extension)
-        return download_file(url, file_path, settings)
-
-
-
-# new download
+# download functions
 def download_file(url, file_path, settings):
     '''Download function with block time outs'''
     try:
@@ -117,6 +53,7 @@ def download_img_file(url, sub_dir, settings):
             f.write(r.content)
     return Outcome(True, '')
 
+# single file functions
 def delete_file(file_path):
     '''Deletes a file'''
     try:
@@ -125,20 +62,12 @@ def delete_file(file_path):
     except OSError as e:
         return Outcome(False, file_path + ': ' + str(e))
 
-def delete_sub(conf, title, reset=False):
-    '''Delete subscription files (optionally including history)'''
-    sub_dir = os.path.join(conf.xml.settings.base_dir.text, title)
-    db_file = os.path.join(conf.paths.db_dir, title)
-    try:
-        shutil.rmtree(sub_dir)
-    except FileNotFoundError:
-        pass
-    if reset:
-        try:
-            os.remove(db_file)
-        except FileNotFoundError:
-            pass
+def verify_file(entry):
+    '''Check to see if recorded file exists or has been removed'''
+    isfile = os.path.isfile(entry['poca_abspath'])
+    return Outcome(isfile, entry['poca_abspath'] + ' exists: ' + str(isfile))
 
+# directory functions
 def check_path(check_dir):
     '''Create a directory'''
     if os.path.isdir(check_dir):
@@ -152,7 +81,18 @@ def check_path(check_dir):
     except OSError:
         return Outcome(False, check_dir + ': Dir could not be created.')
 
-def verify_file(entry):
-    '''Check to see if recorded file exists or has been removed'''
-    isfile = os.path.isfile(entry['poca_abspath'])
-    return Outcome(isfile, entry['poca_abspath'] + ' exists: ' + str(isfile))
+
+# subscription reset
+def delete_sub(conf, title, reset=False):
+    '''Delete subscription files (optionally including history)'''
+    sub_dir = os.path.join(conf.xml.settings.base_dir.text, title)
+    db_file = os.path.join(conf.paths.db_dir, title)
+    try:
+        shutil.rmtree(sub_dir)
+    except FileNotFoundError:
+        pass
+    if reset:
+        try:
+            os.remove(db_file)
+        except FileNotFoundError:
+            pass
